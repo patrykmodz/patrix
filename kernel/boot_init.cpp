580aeea7d99307506388301eb656f7699065115f
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-15 22:55:59 +0100

    test gdt initialization

--- kernel/boot_init.cpp
+++ kernel/boot_init.cpp
@@ -1,16 +1,13 @@
 #include "kernel/boot_init.h"
 #include "kernel/drivers/vga.h"
 #include "kernel/arch/x86/io.h"
+#include "kernel/arch/x86/gdt.h"
 
 void boot_init() {
     vga_init();
-    vga_hide_cursor();
-    vga_write_string("lorem ipsum");
+    //initialize the global descriptor table.
+    gdt_init();
 
-    for (volatile unsigned long i = 0; i < 3000000; i++) {
-    }
-
-    vga_clear();
-    vga_show_cursor();
-    vga_update_cursor();
+    //show that execution continued after loading the gdt.
+    vga_write_string("gdt loaded\n");
 }
