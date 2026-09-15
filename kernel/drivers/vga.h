e95c2f0027f8ce714ac3d4682b6798e44ea93e68
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-15 19:37:51 +0100

    add comments and vga_write_string function

--- kernel/drivers/vga.h
+++ kernel/drivers/vga.h
@@ -1,4 +1,8 @@
 #pragma once
 
+//initialize vga.
 void vga_init();
+//write a character to the screen.
 void vga_write_char(char character);
+//write a string of text to the screen.
+void vga_write_string(const char* string);
