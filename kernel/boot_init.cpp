85e21d0d3aee6da34c4cc28b820ae1a49440611a
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-15 20:15:24 +0100

    test vga newline handling

--- kernel/boot_init.cpp
+++ kernel/boot_init.cpp
@@ -3,5 +3,7 @@
 
 void boot_init() {
     vga_init();
-    vga_write_string("lorem ipsum dolor sit amet");
+    vga_write_string("lorem ipsum");
+    vga_write_char('\n');
+    vga_write_string("dolor sit amet");
 }
