3771f7a7ea8129b16169c27952881544cfdf8853
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-15 20:48:56 +0100

    add vga_scroll() function

--- kernel/drivers/vga.h
+++ kernel/drivers/vga.h
@@ -6,3 +6,5 @@ void vga_init();
 void vga_write_char(char character);
 //write a string of text to the screen.
 void vga_write_string(const char* string);
+//move all screen lines up by one line.
+void vga_scroll();
