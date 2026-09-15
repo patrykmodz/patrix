eb04a2cc0befb0ec43d8f80a8ad4c8a2e4140bc0
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-15 21:53:57 +0100

    test vga cursor control

--- kernel/boot_init.cpp
+++ kernel/boot_init.cpp
@@ -1,9 +1,16 @@
 #include "kernel/boot_init.h"
 #include "kernel/drivers/vga.h"
+#include "kernel/arch/x86/io.h"
 
 void boot_init() {
     vga_init();
+    vga_hide_cursor();
     vga_write_string("lorem ipsum");
-    vga_write_char('\n');
-    vga_write_string("dolor sit amet");
+
+    for (volatile unsigned long i = 0; i < 3000000; i++) {
+    }
+
+    vga_clear();
+    vga_show_cursor();
+    vga_update_cursor();
 }
