d82aa40d1a2f65ce77ddc0b9aece3ce9726ba822
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-15 22:33:37 +0100

    add inb interface

--- kernel/arch/x86/io.h
+++ kernel/arch/x86/io.h
@@ -1,4 +1,6 @@
 #pragma once
 
+//read an 8-bit value from an x86 i/o port.
+unsigned char inb(unsigned short port);
 //write an 8-bit value to an x86 I/O port.
 void outb(unsigned short port, unsigned char value);
