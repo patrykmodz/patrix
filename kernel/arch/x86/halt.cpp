70507b14acb025549b1e074e6d5b9c3dc327e8d7
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-16 19:04:16 +0100

    add kernel halt implementation

---
+++ kernel/arch/x86/halt.cpp
@@ -0,0 +1,13 @@
+#include "kernel/arch/x86/halt.h"
+
+//halt the cpu indefinitely.
+[[noreturn]] void kernel_halt() {
+
+    //disable interrupts before halting the cpu.
+    asm volatile ("cli");
+
+    //halt the cpu until the next interrupt.
+    while (true) {
+        asm volatile ("hlt");
+    }
+}
