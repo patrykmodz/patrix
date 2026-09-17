7e7ece25db136f52020274b15c6f358b66fc9baf
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-17 20:41:35 +0100

    add keyboard interrupt vector code clean up

--- kernel/arch/x86/idt.cpp
+++ kernel/arch/x86/idt.cpp
@@ -1,6 +1,8 @@
 #include "kernel/arch/x86/idt.h"
 #include "kernel/drivers/vga.h"
 #include "kernel/panic.h"
+#include "kernel/arch/x86/pic.h"
+#include "kernel/drivers/keyboard.h"
 
 
 //store the interrupt descriptor table entries.
@@ -29,7 +31,25 @@ void idt_set_entry(
 }
 
 
-//initialize the interrupt descriptor table.
+void idt_set_entries() {
+    //install the divide error handler at interrupt vector 0.
+    idt_set_entry(
+        0,
+        (unsigned int)interrupt_stub_0,
+        0x08,
+        0x8E
+    );
+
+    //install the keyboard interrupt handler at interrupt vector 0x21.
+    idt_set_entry(
+        0x21,
+        (unsigned int)interrupt_stub_33,
+        0x08,
+        0x8E
+    );
+}
+
+
 void idt_init() {
     //set the size of the idt in bytes, minus one.
     idt_pointer.limit = sizeof(idt) - 1;
@@ -45,26 +65,27 @@ void idt_init() {
         idt[i].flags = 0;
         idt[i].base_high = 0;
     }
-
-    //install the divide error handler at interrupt vector 0.
-    idt_set_entry(
-        0,
-        (unsigned int)interrupt_stub_0,
-        0x08,
-        0x8E
-    );
+    //set all entries.
+    idt_set_entries();
 
     //load the idt into the cpu.
     idt_flush((unsigned int)&idt_pointer);
 }
 
 
-//handle interrupts after the processor state has been saved.
 extern "C" void interrupt_handler(InterruptFrame* frame) {
     //handle a divide error.
     if (frame->interrupt_number == 0) {
         //halt the kernel after a divide error.
         kernel_panic("divide error");
+    }
+
+    //handle a keyboard interrupt.
+    if (frame->interrupt_number == 0x21) {
+        //call the keybaord interrupt
+        keyboard_interrupt();
+        //signal that the keyboard interrupt has been handled.
+        pic_eoi(1);
     }
 }
 
