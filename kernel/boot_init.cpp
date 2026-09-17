96d061ac17022f75a30bfc05f6d479d5cd67c429
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-17 20:56:38 +0100

    add pic verification

--- kernel/boot_init.cpp
+++ kernel/boot_init.cpp
@@ -4,13 +4,14 @@
 #include "kernel/boot_status.h"
 #include "kernel/arch/x86/idt.h"
 #include "kernel/arch/x86/halt.h"
+#include "kernel/arch/x86/pic.h"
 
 
 void boot_init() {
 const char* vga_description = "vga initialization";
 const char* gdt_description = "gdt initialization";
 const char* idt_description = "idt initialization";
-
+const char* pic_description = "pic initialization";
 
     //initialize the vga driver.
     vga_init();
@@ -39,6 +40,16 @@ const char* idt_description = "idt initialization";
         boot_status(BootStatus::OK, idt_description);
     } else {
         boot_status(BootStatus::FAILED, idt_description);
+        return;
+    }
+
+    //initialize the pic
+    pic_init();
+
+    if (idt_verify()) {
+        boot_status(BootStatus::OK, pic_description);
+    } else {
+        boot_status(BootStatus::FAILED, pic_description);
         return;
     }
 }
