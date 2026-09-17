d9aca9ff670dd2e43aa128f0ff27f260307d44c8
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-17 17:26:20 +0100

    change project filenames kernel_panic to panic

--- kernel/panic.cpp
+++ kernel/panic.cpp
@@ -1,13 +1,13 @@
-#include "kernel/kernel_panic.h"
+#include "kernel/panic.h"
 #include "kernel/drivers/vga.h"
 #include "kernel/arch/x86/halt.h"
 
 //halt the kernel after an unrecoverable error.
 [[noreturn]] void kernel_panic(const char* reason) {
     //display the kernel panic message.
-    vga_write_string("\nkernel panic: ");
+    vga_str("\nkernel panic: ");
     //display the reason for the panic.
-    vga_write_string(reason);
+    vga_str(reason);
     //halt the processor.
     kernel_halt();
 }
