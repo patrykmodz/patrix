3db35dd5b0b0a6c83c1915a468718112e4e4bc65
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-17 20:48:46 +0100

    add pic implementation

---
+++ kernel/arch/x86/pic.cpp
@@ -0,0 +1,53 @@
+#include "kernel/arch/x86/pic.h"
+#include "kernel/arch/x86/io.h"
+
+
+//master pic command port.
+const unsigned short pic_master_command = 0x20;
+//master pic data port.
+const unsigned short pic_master_data = 0x21;
+//slave pic command port.
+const unsigned short pic_slave_command = 0xA0;
+//slave pic data port.
+const unsigned short pic_slave_data = 0xA1;
+
+
+void pic_init() {
+    //start initialization on both pics.
+    outb(pic_master_command, 0x11);
+    outb(pic_slave_command, 0x11);
+
+    //set the master pic interrupt vector offset.
+    outb(pic_master_data, 0x20);
+
+    //set the slave pic interrupt vector offset.
+    outb(pic_slave_data, 0x28);
+
+    //tell the master pic that the slave is connected to irq2.
+    outb(pic_master_data, 0x04);
+
+    //tell the slave pic that it is connected to irq2 on the master.
+    outb(pic_slave_data, 0x02);
+
+    //set both pics to 8086 mode.
+    outb(pic_master_data, 0x01);
+    outb(pic_slave_data, 0x01);
+
+    //UNMASKING
+
+    //keyboard interrupt.
+    outb(pic_master_data, 0xFD);
+}
+
+
+//signal that a hardware interrupt has been handled.
+void pic_eoi(unsigned char irq) {
+
+    //send an eoi to the slave pic if the irq came from the slave.
+    if (irq >= 8) {
+        outb(pic_slave_command, 0x20);
+    }
+
+    //send an eoi to the master pic.
+    outb(pic_master_command, 0x20);
+}
