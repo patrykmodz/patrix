2d22a7ed6f406e39f89a60b81a5079d4dd9cf10c
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-17 20:49:23 +0100

    add hardware interrupt handling

--- kernel/arch/x86/interrupt_stub.s
+++ kernel/arch/x86/interrupt_stub.s
@@ -25,3 +25,27 @@ interrupt_stub_0:
     add $8, %esp
     #return from the interrupt.
     iret
+
+.global interrupt_stub_33
+
+interrupt_stub_33:
+    #push a placeholder error code because keyboard interrupts do not provide one.
+    push $0
+    #push the keyboard interrupt vector number.
+    push $0x21
+    #save the general purpose registers.
+    pusha
+    #pass the current stack pointer to the common interrupt handler.
+    mov %esp, %eax
+    #pass the interrupt frame to the common interrupt handler.
+    push %eax
+    #call the common interrupt handler.
+    call interrupt_handler
+    #remove the interrupt frame argument.
+    add $4, %esp
+    #restore the general purpose registers.
+    popa
+    #remove the interrupt vector number and error code.
+    add $8, %esp
+    #return from the interrupt.
+    iret
