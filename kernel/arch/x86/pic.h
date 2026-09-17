a92df6c279a0bd29f888dfa6d08146365f3fecee
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-17 20:57:15 +0100

    add pic_verify

--- kernel/arch/x86/pic.h
+++ kernel/arch/x86/pic.h
@@ -2,5 +2,9 @@
 
 //initialize the programmable interrupt controller.
 void pic_init();
+
 //signal that a hardware interrupt has been handled.
 void pic_eoi(unsigned char irq);
+
+//verify that the programmable interrupt controller was initialized correctly.
+bool pic_verify();
