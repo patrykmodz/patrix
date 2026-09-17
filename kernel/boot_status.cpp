c8e5dbaf595bce7a8f2a7ae11b41460a2c3cf1d4
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-17 17:28:13 +0100

    fix vga function names

--- kernel/boot_status.cpp
+++ kernel/boot_status.cpp
@@ -5,22 +5,18 @@
 void boot_status(BootStatus status, const char* name) {
     //display a successful initialization step.
     if (status == BootStatus::OK) {
-        vga_write_string("[ OK ] ");
+        vga_str("[ OK ] ");
     }
-
     //display a warning for an initialization step.
     else if (status == BootStatus::WARNING) {
-        vga_write_string("[ WARN ] ");
+        vga_str("[ WARN ] ");
     }
-
     //display a failed initialization step.
     else if (status == BootStatus::FAILED) {
-        vga_write_string("[ FAIL ] ");
+        vga_str("[ FAIL ] ");
     }
-
     //display the name of the initialization step.
-    vga_write_string(name);
-
+    vga_str(name);
     //move to the next line.
-    vga_write_char('\n');
+    vga_char('\n');
 }
