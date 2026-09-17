1ed5b67c0570500a287191540cee1893639005df
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-17 17:30:51 +0100

    shorten vga function names

--- kernel/drivers/vga.h
+++ kernel/drivers/vga.h
@@ -6,15 +6,15 @@ void vga_init();
 bool vga_verify();
 
 //write a character to the screen.
-void vga_write_char(char character);
+void vga_char(char character);
 //write a string of text to the screen.
-void vga_write_string(const char* string);
+void vga_str(const char* string);
 //move all screen lines up by one line.
 void vga_scroll();
 //clear the entire screen.
 void vga_clear();
 
 //hardware-cursor
-void vga_update_cursor();
-void vga_hide_cursor();
-void vga_show_cursor();
+void vga_updatecurs();
+void vga_hcurs();
+void vga_scurs();
