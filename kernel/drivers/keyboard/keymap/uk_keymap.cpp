bf16bc43889811342c13507f5861838eb3222709
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-18 22:37:38 +0100

    add simple uk_keymap implementation

---
+++ kernel/drivers/keyboard/keymap/uk_keymap.cpp
@@ -0,0 +1,33 @@
+#include "drivers/keyboard/scancodes.h"
+#include "drivers/keyboard/keymap/keymap.h"
+
+KEY ukmap[] = {
+    {key_type::CHARACTER, key_modifier::BASE, KEY_A, 'a'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_B, 'b'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_C, 'c'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_D, 'd'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_E, 'e'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_F, 'f'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_G, 'g'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_H, 'h'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_I, 'i'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_J, 'j'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_K, 'k'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_L, 'l'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_M, 'm'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_N, 'n'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_O, 'o'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_P, 'p'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_Q, 'q'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_R, 'r'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_S, 's'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_T, 't'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_U, 'u'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_V, 'v'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_W, 'w'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_X, 'x'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_Y, 'y'},
+    {key_type::CHARACTER, key_modifier::BASE, KEY_Z, 'z'},
+
+    {key_type::CHARACTER, key_modifier::BASE, KEY_SPACE, ' '}
+};
