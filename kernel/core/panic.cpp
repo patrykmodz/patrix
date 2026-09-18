186c0bc0628f4b37edf52c1dfb6f9294792f5c74
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-18 17:00:18 +0100

    move core file to core directory change include header paths * fix kernel panic text being one line further down

--- kernel/core/panic.cpp
+++ kernel/core/panic.cpp
@@ -1,6 +1,6 @@
-#include "kernel/panic.h"
-#include "kernel/drivers/vga.h"
-#include "kernel/arch/x86/halt.h"
+#include "core/panic.h"
+#include "drivers/vga.h"
+#include "arch/x86/halt.h"
 
 //halt the kernel after an unrecoverable error.
 [[noreturn]] void kernel_panic(const char* reason) {
@@ -8,7 +8,7 @@
     vga_clear();
     VGA_COLOUR = VGA_LIGHT_BLUE;
 
-    vga_str("\nkernel panic: ");
+    vga_str("kernel panic: ");
     vga_str(reason);
 
     kernel_halt();
