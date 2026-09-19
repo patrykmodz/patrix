467e8f898fda2e0a2eb6e938236ef610d2112dd2
patrykmodz <contact.patrykmodz@gmail.com>
2026-09-19 01:24:32 +0100

    add vgacon interfaces

--- kernel/drivers/vga/vgacon.h
+++ kernel/drivers/vga/vgacon.h
@@ -3,3 +3,11 @@
 void vgacon_init();
 bool vgacon_verify();
 void vgacon_char(char character);
+void vgacon_bck();
+void vgacon_str(const char* string);
+void vgacon_syncurs();
+void vgacon_clear();
+
+
+extern int curs_x;
+extern int curs_y;
